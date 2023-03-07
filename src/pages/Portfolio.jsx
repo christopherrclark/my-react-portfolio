bc1a55c1620ea0de31c8906b318288f4ece6a15c
Fozzy Yeti <christopherrclark@gmail.com>
2023-03-06 20:30:38 -0600

    added images and started portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import gamersHavenImg from '../images/gamers-haven-screenshot.png'
+
+
 
 const Portfolio = () => {
+
+  const projects = [
+    {id: 1, name: 'Gamers Haver', image: gamersHavenImg, deployed: 'https://gamers-haven.herokuapp.com/', repo: 'https://github.com/christopherrclark/gaming-blog', techStack: [{name: "Express"}, {name: "MySQL2"}, {name: "Sequelize"}, {name: ""}, {name: "Node"}, {name: "Bootstrap"}, {name: "JavaScript"}
+  ]},
+  ]
+
   return (
-    <div>
-      Portfolio
+    <div className="big-project-container">
+      {projects.map(project => (
+        <div className="project-container" key={project.id}>
+          <div className="project-image-container">
+            <a href={project.deployed} target="blank">
+              <img className="project-image" src={project.image} alt={project.name}/>
+            </a>
+          </div>
+
+          <div className="project-text-container">
+            <h4 className="project-name">{project.name}</h4>
+            <div className="project-text">
+              <div className="project-links">
+                <div>
+                  <a className='project-link' href={project.deployed} target="blank">Deployed Page</a>
+                </div>
+                <div>
+                  <a className='project-link' href={project.repo} target="blank">GitHub</a>
+                </div>
+              </div>
+              <div className="tech-stack">
+                {project.techStack?.map(tech => (
+                  <p className="tech-stack-item" key={tech.name}>
+                    {tech.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
